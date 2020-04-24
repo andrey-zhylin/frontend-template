106d3969d357a272d54e2513b7b9623d2593e7e2
Andrey Zhylin <andrey.zhylin@outlook.com>
2020-04-24 09:48:51 +0300

    basic contentful setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,37 @@
+let contentfulConfig;
+
+try {
+  // Load the Contentful config from the .contentful.json
+  contentfulConfig = require('./.contentful')
+} catch (_) {
+  throw new Error(
+    'Contentful spaceId and the delivery token need to be provided.'
+  );
+}
+
+// Overwrite the Contentful config with environment variables if they exist
+contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID || contentfulConfig.spaceId,
+  accessToken:
+    process.env.CONTENTFUL_DELIVERY_TOKEN || contentfulConfig.accessToken
+};
+
+const { spaceId, accessToken } = contentfulConfig;
+
+if (!spaceId || !accessToken) {
+  throw new Error(
+    'Contentful spaceId and the delivery token need to be provided.'
+  );
+}
+
 module.exports = {
-  plugins: [`gatsby-plugin-ts`]
+  pathPrefix: '/gatsby-contentful-starter',
+  plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: contentfulConfig
+    },
+    'gatsby-plugin-typescript',
+    'gatsby-plugin-graphql-codegen'
+  ]
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import { ProductsQueryQuery } from '../../graphql-types';
 
-import { Header } from '../components';
+const RootIndex = ({ data }: { data: ProductsQueryQuery }) => {
+  const products = data.allContentfulProduct.edges;
+  const blocks = data.allContentfulBlock.edges;
 
-export const Index = () => <Header />;
+  return (
+    <div>
+      <ul>
+        {products.map(({ node }) => {
+          return <li key={node.name || ''}>{node.name}</li>;
+        })}
+      </ul>
+      {blocks.map(({ node }) => {
+        return (
+          <pre key="test">{JSON.stringify(node.body?.json, undefined, 2)}</pre>
+        );
+      })}
+    </div>
+  );
+};
 
-export default Index;
+export default RootIndex;
+
+export const pageQuery = graphql`
+  query ProductsQuery {
+    allContentfulProduct {
+      edges {
+        node {
+          name
+        }
+      }
+    }
+    allContentfulBlock {
+      edges {
+        node {
+          body {
+            body
+            json
+          }
+        }
+      }
+    }
+  }
+`;
